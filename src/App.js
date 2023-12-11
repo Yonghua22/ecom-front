97e3ab12f0ac7bdad95c988f816b7499b4149a74
Yonghua22 <yfyfjade@gmail.com>
2023-12-10 19:56:53 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,21 +1,83 @@
-import { Container } from 'react-bootstrap';
-import Header from './components/Header';
-import Footer from './components/Footer';
+// import { Container } from 'react-bootstrap';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+
+// const App = () => {
+//   return (
+//     <>
+//       {/* <ToastContainer /> */}
+//       <Header />
+//       <main className='py-3'>
+//         <Container>
+//           <h1>Welcome To Proshop</h1>
+//         </Container>
+//       </main>
+//       <Footer />
+//     </>
+//   );
+  
+// };
+
+// export default App;
+// import {addBlankLine} from './addBlankLine';
+
+
+// Import React
+const React = require('react');
+const { Container, Row, Col, Carousel, Card } = require('react-bootstrap');
+
+// Assume that 'products' is an array of product objects
+const products = require('./sources/product.json');
+
+// Define App component using React.createElement
 
 const App = () => {
-  return (
-    <>
-      {/* <ToastContainer /> */}
-      <Header />
-      <main className='py-3'>
-        <Container>
-          <h1>Welcome To Proshop</h1>
-        </Container>
-      </main>
-      <Footer />
-    </>
+  // addBlankLine();
+  return React.createElement(Container, null,
+    React.createElement(Carousel, { pause: 'hover', className: 'bg-dark' },
+      // Carousel items would be mapped from a data source here
+
+      React.createElement(Carousel.Item, null,
+        React.createElement('img', {
+          src: '',
+          alt: 'First slide',
+          className: 'd-block w-100'
+        }),
+        React.createElement(Carousel.Caption, { className: 'carousel-caption' },
+          React.createElement('h2', null, 'Logitech G-Series Gaming Mouse ($39.99)')
+        )
+      )
+      // More Carousel.Items
+    ),
+    React.createElement('h1', null, 'Latest Products'),
+    React.createElement(Row, null,
+      products.map(product => React.createElement(Col, {
+          key: product._id,
+          sm: 12,
+          md: 6,
+          lg: 4,
+          xl: 3
+        },
+        React.createElement(Card, { className: 'my-3 p-3 rounded' },
+          React.createElement('a', { href: '/' },
+            React.createElement(Card.Img, { src: product.image, variant: 'top' })
+          ),
+          React.createElement(Card.Body, null,
+            React.createElement('a', { href: `/product/${product._id}` },
+              React.createElement(Card.Title, { as: 'div' },
+                React.createElement('strong', null, product.name)
+              )
+            ),
+            React.createElement(Card.Text, { as: 'div' },
+              // Rating component
+            ),
+            React.createElement(Card.Text, { as: 'h3' }, `$${product.price}`)
+          )
+        )
+      ))
+    )
   );
-  
 };
 
-export default App;
+// Export the component
+module.exports = App;
---
+++ src/addBlankLine.js
@@ -0,0 +1,7 @@
+function addBlankLine() {
+    var container = document.getElementById('blankSpaceContainer');
+    var blankLine = document.createElement('div');
+    blankLine.style.height = '50px'; // 设置空白行的高度
+    container.appendChild(blankLine);
+}
+
