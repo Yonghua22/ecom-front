1ac7a0032ff5da9cee162c006cacb1dba73c389b
Yonghua22 <yfyfjade@gmail.com>
2023-12-10 18:05:58 -0800

    version update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-
-
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-  return <h1>Welcome to proshop</h1>
+  return (
+    <>
+      {/* <ToastContainer /> */}
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
   
 };
 
