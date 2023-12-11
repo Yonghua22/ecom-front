5a29bbc600b3fe10b82dff54f2d32a0e0d8f9c24
Yonghua22 <yfyfjade@gmail.com>
2023-12-10 21:45:33 -0800

    upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png';
-
+import { LinkContainer } from 'react-router-bootstrap';
 const Header = ()  => {
     // return(
     //     <header>
@@ -70,8 +70,11 @@ const Header = ()  => {
       <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
+          <LinkContainer to='/'>
+          < img src={logo} alt='ProShop' />
+          </LinkContainer>
         <Navbar.Brand href = "/">ProShop</Navbar.Brand>
-        < img src={logo} alt='ProShop' />
+       
         {/* ProShop
          </Navbar.Brand> */}
         
